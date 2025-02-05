904127742c1343d4130f785d77824eee78137a27
Princy-Patel-30 <princy.patel@bacancy.com>
2025-02-05 11:02:26 +0530

    commit

--- sync.js
+++ sync.js
@@ -4,7 +4,6 @@ const path = require('path');
 const PORT = 5000;
 const filepath = path.join(__dirname, 'data.json');
 
-// Function to read JSON data synchronously
 function readData() {
     try {
         return JSON.parse(fs.readFileSync(filepath, 'utf-8'));
@@ -13,29 +12,27 @@ function readData() {
     }
 }
 
-// Function to write JSON data synchronously
 function writeData(data) {
     fs.writeFileSync(filepath, JSON.stringify(data, null, 2));
 }
 
-// Function to send response
 function sendResponse(res, statusCode, data) {
     res.writeHead(statusCode, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify(data));
 }
 
-// Handle GET request
+//get method
 function getData(res) {
     const data = readData();
     sendResponse(res, 200, data);
 }
 
-// Handle POST request
+//post method
 function postData(res, body) {
     const data = readData();
     const bodyData = JSON.parse(body);
     const newPost = {
-        id: data.length + 1,
+        id: data.length ? data[data.length - 1].id + 1 : 1,
         title: bodyData.title,
         body: bodyData.body
     };
@@ -44,24 +41,41 @@ function postData(res, body) {
     sendResponse(res, 201, newPost);
 }
 
-// Handle PUT request
+//put method
 function updateData(res, id, body) {
     let data = readData();
     const bodyData = JSON.parse(body);
     const index = data.findIndex(post => post.id === id);
+
     if (index !== -1) {
-        data[index] = { ...data[index], ...bodyData };
+        data[index].title = bodyData.title;
+        data[index].body = bodyData.body;
         writeData(data);
         sendResponse(res, 200, data[index]);
     } else {
-        const newPost = { id, ...bodyData };
+        const newPost = { id, title: bodyData.title, body: bodyData.body };
         data.push(newPost);
         writeData(data);
         sendResponse(res, 201, newPost);
     }
 }
 
-// Handle DELETE request
+//patch method
+function patchData(res, id, body) {
+    let data = readData();
+    const bodyData = JSON.parse(body);
+    const index = data.findIndex(post => post.id === id);
+
+    if (index !== -1) {
+        data[index] = { ...data[index], ...bodyData };
+        writeData(data);
+        sendResponse(res, 200, data[index]);
+    } else {
+        sendResponse(res, 404, { message: 'Post not found' });
+    }
+}
+
+//delete method
 function deleteData(res, id) {
     let data = readData();
     const newData = data.filter(post => post.id !== id);
@@ -73,6 +87,7 @@ function deleteData(res, id) {
     }
 }
 
+//server 
 const server = http.createServer((req, res) => {
     if (req.method === 'GET' && req.url === '/posts') {
         getData(res);
@@ -85,6 +100,11 @@ const server = http.createServer((req, res) => {
         let body = '';
         req.on('data', chunk => body += chunk);
         req.on('end', () => updateData(res, id, body));
+    } else if (req.method === 'PATCH' && req.url.startsWith('/posts/')) {
+        const id = parseInt(req.url.split('/')[2]);
+        let body = '';
+        req.on('data', chunk => body += chunk);
+        req.on('end', () => patchData(res, id, body));
     } else if (req.method === 'DELETE' && req.url.startsWith('/posts/')) {
         const id = parseInt(req.url.split('/')[2]);
         deleteData(res, id);
